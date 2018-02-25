b95825b889fdda5aa7e16a3066e7075c598701a7
Mariano Schemmari <marianosche@gmail.com>
2018-02-25 20:05:36 -0300

    archivos que faltaban

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import ActionButton from './button.js';
 import ItemImg from './image.js';
 
+
 class MovieBox extends Component {
   render() {
     return (
       <div>
         <article className="col-md-6">
           <div className="list-item">
-    ``        <ItemImg />
+            <ItemImg />
             <div className="list-item-body">
-                <h3 className="list-item-title">Thor: Ragnarok</h3>
+                <h3 className="list-item-title">{this.props.name}</h3>
                 <div className="list-item-description">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam blanditiis consequuntur corporis distinctio doloremque, eum expedita id in ipsam minus modi molestias optio perferendis quam quas repudiandae sint. Suscipit, veritatis!</p>
                 </div>
