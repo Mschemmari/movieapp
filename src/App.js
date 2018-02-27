e4abe66152bad86e9229d4e27427bfb1cc733612
Mariano Schemmari <marianosche@gmail.com>
2018-02-27 14:34:46 -0300

    resolved issues

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieBox from './MovieBox.js';
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.css'
-import 'material-icons/css/material-icons.min.css'
-=======
+// import 'material-icons/css/material-icons.min.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
->>>>>>> b95825b889fdda5aa7e16a3066e7075c598701a7
-// import Axios from 'axios';
 
-
-// Axios.get('https://pokeapi.co/api/v2/pokemon/')
-//    .then((response)=>console.log(response))
-//    .catch((error)=>console.log(error));
-<<<<<<< HEAD
-=======
-
-const movies = [
- {
- name: "Thor Ragnarok",
- },
- {
- name: "Back to the Future",
- },
- {
- name: "Robocop",
- },
-];
->>>>>>> b95825b889fdda5aa7e16a3066e7075c598701a7
 
 var movies = [
  {
@@ -45,27 +22,15 @@ var movies = [
 ];
 class App extends Component {
   render() {
-<<<<<<< HEAD
     const ItemsSection = movies.map((movies)=>
       <MovieBox titulo={movies.name} key={movies.name}/>
     );
     return (
-          <div className="App">
-            <div className="container">
-              <div className="row">
-                {ItemsSection}
-              </div>
-            </div>
+        <div className="container">
+          <div className="row">
+            {ItemsSection}
           </div>
-=======
-    const ItemsSection = movies.map((movie)=>
-      <MovieBox titulo={movies.name} key={movie}/>
-    );
-    return (
-      <div className="App">
-      <div>{ItemsSection}</div>
-      </div>
->>>>>>> b95825b889fdda5aa7e16a3066e7075c598701a7
+        </div>
     );
   }
 }
--- src/image.js
+++ src/image.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class ItemImg extends Component {
   render() {
@@ -10,7 +10,8 @@ class ItemImg extends Component {
     );
   }
 }
-ItemImg.propTypes = {
-  children: PropTypes.element.isRequired
-};
+// ItemImg.propTypes = {
+//   children: PropTypes.element.isRequired
+// };
+
 export default ItemImg;
